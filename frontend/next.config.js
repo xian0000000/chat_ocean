692e4be82711319f5fb2051358d964d8f28ffcb4
xian0000000 <emmachan00000@gmail.com>
2026-03-19 10:09:40 +0700

    init

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,9 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    cache: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true, // optimasi minify
 };
 
+module.exports = nextConfig;
 module.exports = nextConfig;
 module.exports = nextConfig
